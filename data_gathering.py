617f2d354d8be5ad73d9d0bd88c13b6362daed93
Aukkawut Ammartayakun <aukkawut@github.com>
2021-07-26 21:12:40 +0700

    add feature to save experiment data

--- data_gathering.py
+++ data_gathering.py
@@ -14,6 +14,7 @@ import numpy as np
 #import time
 
 #change these initial condition
+save_experiment = False
 timestep = 10000
 iteration = 30
 mal_fraction = 0.5
@@ -33,13 +34,17 @@ for i in range(10):
     mal_fraction = 0.5
     for j in range(10):
         print("subexperiment " + str(j)+"/10")
-        f = open("AntSimData4.csv","w")
+        if(save_experiment):
+            name = "exp"+str(i+1)+"_"+str(j+1)+".csv"
+        else:
+            name = "AntSimDataBuf.csv"
+        f = open(name,"w")
         subprocess.run(['./build/AntSimulator', str(timestep), str(iteration), str(mal_fraction), str(mal_timer), str(mal_focus), str(tracing_pattern), str(counter_pheromone), str(fake_intensity), str(display_GUI), str(metric_mode),str(1)], stdout = f)
         #this subprocess will yield one csv file
         #what we want is something like this column = [exp1, exp2, exp3, exp4, ...], each row represents the time step
         #with this format, we can, ideally, get n csv files (where n is number of experiments required to be done by another variable) and merge it again.
         f.close()
-        data = pd.read_csv('AntSimData4.csv', header=None).T #this will transpose the dataframe
+        data = pd.read_csv(name, header=None).T #this will transpose the dataframe
         #create average column
         data['mean'] = data.mean(axis=1)
         #sample the data at t = timestep/2 (note that bin size is 10).
