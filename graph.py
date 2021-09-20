3d056971f93db2da79d8f31d7372f067b96ef7a3
Ashay Aswale <asaswale@wpi.edu>
2021-09-20 02:59:02 -0400

    Additional graphs for detractor ants

--- graph.py
+++ graph.py
@@ -2,10 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./data/AntSimData_SIM_STEPS-50000_SIM_ITERS-1_mal_frac-0.500000_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-1.000000_hell_phermn_evpr-1.000000_iter-0.csv", header=None).T
+mal_frac = str(("%.6f" % (2**-10)))
+hell_evpr_multi = str(1000)
+filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-1_mal_frac-"+mal_frac+"_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-1.000000_hell_phermn_evpr-"+hell_evpr_multi+".000000_iter-0.csv"
+foldername = "./data_exp-mal_frac-mal_evpr_rate-one_iter/"
+data = pd.read_csv(foldername+filename,header=None).T
 data = np.array(data)
 plt.plot(data.T)
 plt.xlabel('iterations')
 plt.ylabel('cumulative food count/# of benevolent ants')
-plt.savefig("graph.png")
+plt.savefig(foldername+"graphs/"+filename+".png")
 
