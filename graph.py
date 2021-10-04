7af97f27d3b8bc7452ac87bdc4d8b855b487bc9d
Ashay Aswale <asaswale@wpi.edu>
2021-10-04 15:40:00 -0400

    graphing with different line style

--- graph.py
+++ graph.py
@@ -8,8 +8,14 @@ filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-1_mal_frac-"+mal_frac+"_mal_del
 foldername = "./data_exp-mal_frac-mal_evpr_rate-one_iter/"
 data = pd.read_csv(foldername+filename,header=None).T
 data = np.array(data)
-plt.plot(data.T)
-plt.xlabel('iterations')
-plt.ylabel('cumulative food count/# of benevolent ants')
+data = data.T
+# print(data)
+plt.plot(data[:,0], 'b', linestyle='-', label='Food bits per ant \ncollected from source')
+plt.plot(data[:,1], 'm', linestyle='-.', label='Food bits per ant \ndelivered to the nest​')
+plt.plot(data[:,2], 'r', linestyle='--', label='Fraction of ants that \ncollected food from source​')
+plt.plot(data[:,3], 'k', linestyle=':', label='Fraction of ants that \ndelivered food to the nest​')
+# plt.legend(fontsize=15)
+# plt.xlabel('iterations')
+# plt.ylabel('cumulative food count/# of benevolent ants')
 plt.savefig(foldername+"graphs/"+filename+".png")
 
