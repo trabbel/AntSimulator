8bf026ef3cff627967f9de0c82fdaf2b5c2f332b
Aukkawut Ammartayakun <50354662+aukkawut@users.noreply.github.com>
2021-10-04 23:00:37 -0400

    update graph.py
    make 2 y axis

--- graph.py
+++ graph.py
@@ -1,21 +1,36 @@
+from mpl_toolkits.axes_grid1 import host_subplot
+from mpl_toolkits import axisartist
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
-mal_frac = str(("%.6f" % (2**-10)))
-hell_evpr_multi = str(1000)
-filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-1_mal_frac-"+mal_frac+"_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-1.000000_hell_phermn_evpr-"+hell_evpr_multi+".000000_iter-0.csv"
-foldername = "./data_exp-mal_frac-mal_evpr_rate-one_iter/"
+#initialize paramenter here
+plt.rcParams["figure.figsize"] = (12,10)
+plt.rcParams["axes.labelsize"] = 16
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+mal_frac = str(("%.6f" % (2**-1)))
+hell_evpr_multi = str(100)
+filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-1_mal_frac-"+mal_frac+"_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-1_hell_phermn_intens-1.000000_hell_phermn_evpr-"+hell_evpr_multi+".000000_iter-0.csv"
+foldername = "./data/"
 data = pd.read_csv(foldername+filename,header=None).T
 data = np.array(data)
-data = data.T
-# print(data)
-plt.plot(data[:,0], 'b', linestyle='-', label='Food bits per ant \ncollected from source')
-plt.plot(data[:,1], 'm', linestyle='-.', label='Food bits per ant \ndelivered to the nest​')
-plt.plot(data[:,2], 'r', linestyle='--', label='Fraction of ants that \ncollected food from source​')
-plt.plot(data[:,3], 'k', linestyle=':', label='Fraction of ants that \ndelivered food to the nest​')
-# plt.legend(fontsize=15)
-# plt.xlabel('iterations')
-# plt.ylabel('cumulative food count/# of benevolent ants')
-plt.savefig(foldername+"graphs/"+filename+".png")
-
+host = host_subplot(111, axes_class=axisartist.Axes)
+plt.subplots_adjust(right=0.75)
+par1 = host.twinx()
+par1.axis["right"].toggle(all=True)
+#define which one will get plot
+l1,l2 = host.plot(data[0:2].T) #this is the left axis data
+l3,l4 = par1.plot(data[2:4].T) #this is the right axis data
+#change the style of line here
+plt.setp(l2,linestyle='--', linewidth=2) 
+plt.setp(l3, linestyle=':', linewidth=2)
+plt.setp(l4, linestyle='-.', linewidth=2)  
+par1.set_ylim(0, 1) #set y range for fraction to be between 0 - 1
+plt.xlabel('iterations') #x axis
+plt.ylabel('Food bits per ants', fontsize=20) #left axis
+par1.set_ylabel("Fraction of ants", fontsize=20) #right axis
+par1.legend()
+host.legend([l1,l2],['Food bits per ant collected from source','Food bits per ant delivered food to nest']) #change legends here for left data
+par1.legend([l3,l4],['Fraction of ants that collected food from source', 'Fraction of ants that delivered food to nest']) #change legends here for right data
+plt.savefig(foldername+"graphs/"+filename+".png",dpi = 400, facecolor=host.get_facecolor()) 
+#plt.savefig(foldername+"graphs/1.png",dpi = 400, facecolor=host.get_facecolor()) #modified for windows, need to change back
