639a36048cd9ae2d81a7cd62314ec97518afcfc6
Ashay Aswale <asaswale@wpi.edu>
2021-10-06 19:30:42 -0400

    WIP

--- graph.py
+++ graph.py
@@ -9,12 +9,15 @@ plt.rcParams["axes.labelsize"] = 25
 plt.rcParams['xtick.labelsize'] = 18
 plt.rcParams['ytick.labelsize'] = 18
 plt.rcParams['legend.fontsize'] = 18
-mal_frac = str(("%.6f" % (2**-8)))
-hell_evpr_multi = str(1)
-filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-4_mal_frac-"+mal_frac+"_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-"+hell_evpr_multi+"_hell_phermn_evpr-5.000000_dil_max-500_dil_incr-500_iter-20.csv"
+m = 1
+e = 0
+mal_frac = str(("%.6f" % (2**-m)))
+hell_evpr_multi = str(e)
+filename = "AntSimData_SIM_STEPS-50000_SIM_ITERS-4_mal_frac-"+mal_frac+"_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-1.000000_hell_phermn_evpr-"+hell_evpr_multi+".000000_dil_max-500_dil_incr-500_iter-41.csv"
 foldername = "./data_exp-counters-multi-iteration/"
 data = pd.read_csv(foldername+filename,header=None).T
 data = np.array(data)
+data = (data)/(1-(2**-m))
 host = host_subplot(111, axes_class=axisartist.Axes)
 plt.subplots_adjust(right=0.75)
 par1 = host.twinx()
@@ -24,9 +27,10 @@ l1,l2 = host.plot(data[0:2].T) #this is the left axis data
 data[2:4] *= 100
 l3,l4 = par1.plot(data[2:4].T) #this is the right axis data
 #change the style of line here
-plt.setp(l2,linestyle='--', linewidth=4) 
-plt.setp(l3, linestyle=':', linewidth=4)
-plt.setp(l4, linestyle='-.', linewidth=4)  
+plt.setp(l1,linestyle='-', linewidth=6) 
+plt.setp(l2,linestyle='--', linewidth=6) 
+plt.setp(l3, linestyle=':', linewidth=6)
+plt.setp(l4, linestyle='-.', linewidth=6)  
 # host.set_ylim(-0.0005, 101) 
 # par1.set_ylim(-0.05, 11)
 par1.set_ylim(-5, 100) #set y range for fraction to be between 0 - 1
@@ -36,5 +40,5 @@ par1.set_ylabel("Percentage of cooperators", fontsize=20) #right axis
 # par1.legend()
 # host.legend([l1,l2],['Food bits per ant collected from source','Food bits per ant delivered food to nest']) #change legends here for left data
 # par1.legend([l3,l4],['Successful collectors', 'Successful deliverers'])  #change legends here for right data
-plt.savefig("graphs/5-oct_morning/"+filename+".png",dpi = 400, facecolor=host.get_facecolor())
+plt.savefig("graphs/6-oct_morning/"+filename+".png",dpi = 400, facecolor=host.get_facecolor())
 #plt.savefig(foldername+"graphs/1.png",dpi = 400, facecolor=host.get_facecolor()) #modified for windows, need to change back
--- heatmap.py
+++ heatmap.py
@@ -4,53 +4,56 @@ import pandas as pd
 import seaborn as sns
 import glob
 
-path = r'./data' # use your path
+path = r'./data_exp-counters-multi-iteration' # use your path
 dilusion_max_set = [50, 100, 250, 500, 750, 1000]
 dilusion_increment_power = [1, 2, 5, 10, 50, 100, 1000]
 # evapor_set = evapor_set[::-1]
-all_files = glob.glob(path + "/*ctr_pherm-1*iter-*.csv")
+all_files = glob.glob(path + "/*ctr_pherm-0*iter-*.csv")
 
 spec_data = np.zeros((6,7,4))
 k = 0
-for i, e in enumerate(dilusion_max_set):
-    for j, m in enumerate(dilusion_increment_power):
-        for file in all_files:
-            if (("mal_frac-0.015625") in file) and (("hell_phermn_evpr-1.000000") in file):
-                if ("dil_max-"+str(e)+"_") in file:
-                    if ("dil_incr-"+str((int)(e/m))+"_") in file:
-                        dt = np.array(pd.read_csv(file, header=None))
-                        modified_data = dt
-                        modified_data = (modified_data)/(1-(2**-m))
-                        spec_data[i, j] += dt[-1]/20
-                        k +=1
+# for i, e in enumerate(dilusion_max_set):
+#     for j, m in enumerate(dilusion_increment_power):
+for file in all_files:
+            # if (("mal_frac-0.015625") in file) and (("hell_phermn_evpr-1.000000") in file):
+  if (("AntSimData_SIM_STEPS-50000_SIM_ITERS-4_mal_frac-0.500000_mal_delay-100_mal_ants_focus-1_ant_tracing-1_ctr_pherm-0_hell_phermn_intens-1.000000_hell_phermn_evpr-0.000000_dil_max-500_dil_incr-500_iter-") in file):
+                # if ("dil_max-"+str(e)+"_") in file:
+                #     if ("dil_incr-"+str((int)(e/m))+"_") in file:
+    dt = np.array(pd.read_csv(file, header=None))
+    # modified_data = dt
+    # modified_data = (modified_data)/(1-(2**-m))
+    # spec_data[i, j] += dt[-1]/20
+    # k +=1
+    print(dt[-1], file[-5], file[-6])
+    k += (dt[-1][0])/20
                         # print(file)
                         # continue
 
 print(k)
 # #heatmap axis
-x = np.array(dilusion_max_set)
-# y = [str(n) for n in range(1,11)]
-y = np.array(dilusion_increment_power)
-# x=x[::-1]
-# y=y[::-1]
-color_blue = sns.color_palette("coolwarm_r", as_cmap=True)
-color_green = sns.color_palette("coolwarm_r", as_cmap=True)
+# x = np.array(dilusion_max_set)
+# # y = [str(n) for n in range(1,11)]
+# y = np.array(dilusion_increment_power)
+# # x=x[::-1]
+# # y=y[::-1]
+# color_blue = sns.color_palette("coolwarm_r", as_cmap=True)
+# color_green = sns.color_palette("coolwarm_r", as_cmap=True)
 
-fig,axn = plt.subplots(2, 2, sharex=True, sharey=True)
+# fig,axn = plt.subplots(2, 2, sharex=True, sharey=True)
 
-df=[]
-for i in range(spec_data.shape[-1]):
-    df.append(pd.DataFrame(spec_data[:,:,i]))
+# df=[]
+# for i in range(spec_data.shape[-1]):
+#     df.append(pd.DataFrame(spec_data[:,:,i]))
 
-print(df)
+# print(df)
 
-for i,ax in enumerate(axn.flat):
-    if(i%2==0):
-        df[i] = df[i].rename_axis("Rel Evaporation Multi")
-    if(i>1):
-        df[i] = df[i].rename_axis("Malicious Ants Fraction", axis=1)
-    color = color_blue if i<2 else color_green
-    plot = sns.heatmap(df[i], ax=ax, xticklabels = y, yticklabels = x, cmap=color, cbar=True, vmin=0, vmax=1 if i>1 else 8.5)
-    plt.xticks(rotation = 45)
+# for i,ax in enumerate(axn.flat):
+#     if(i%2==0):
+#         df[i] = df[i].rename_axis("Rel Evaporation Multi")
+#     if(i>1):
+#         df[i] = df[i].rename_axis("Malicious Ants Fraction", axis=1)
+#     color = color_blue if i<2 else color_green
+#     plot = sns.heatmap(df[i], ax=ax, xticklabels = y, yticklabels = x, cmap=color, cbar=True, vmin=0, vmax=1 if i>1 else 8.5)
+#     plt.xticks(rotation = 45)
 
-plot.figure.savefig("graphs/6-oct_morning/heatmap_015625_1.png")
+# plot.figure.savefig("graphs/6-oct_morning/heatmap_015625_1.png")
